9e5ebc00990f92c5967eaeddcc28cd3ead09753d
ranjana980 <ranjana20@navgurukul.org>
2024-03-17 17:28:35 +0530

    feat: slider and footer changes

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ const getData = () => {
         div.className = 'recipe-card'
         let img = document.createElement('img')
         img.src = "/images/image.jpg"
-        img.height = 200
-        img.width = 330
+        img.height = 150
+        img.width = 200
         div.appendChild(img)
         let actionBtn = document.createElement('div')
         actionBtn.className = "action-btn"
+        let spanView=document.createElement('span')
+        spanView.className="view"
+        spanView.addEventListener('click', function (e) {
+            window.location.replace(`/edit.html?${item?.id}`)
+        })
+        spanView.innerHTML = `<i class="material-icons">import_contacts</i>`
+        actionBtn.appendChild(spanView)
+        let meal = document.createElement('h5')
+        meal.textContent = item?.meal?.toLowerCase() || ""
+        actionBtn.appendChild(meal)
         let spanEdit = document.createElement('span');
         spanEdit.className = "edit"
         spanEdit.addEventListener('click', function (e) {
@@ -28,21 +38,9 @@ const getData = () => {
         let h4 = document.createElement('h4')
         h4.textContent = item.name
         div.appendChild(h4)
-        let h5 = document.createElement('h5')
-        h5.textContent = `${item?.ingredients?.split(',')?.length} Ingredients`
-        div.appendChild(h5)
-        let ul = document.createElement('ul')
-        item?.ingredients?.split(',')?.map((ele) => {
-            let li = document.createElement('li')
-            li.textContent = ele;
-            ul.appendChild(li);
-        }
-        )
-        div.appendChild(ul)
         recipeList.appendChild(div)
     })
 }
-
 
 const handleViewItem = (item) => {
     const localData = JSON.parse(localStorage.getItem("data")) || []
@@ -53,19 +51,22 @@ const handleViewItem = (item) => {
     document.getElementsByName('details')[0].value = formData?.details
 }
 
-
 const handleEditItem = () => {
     const id = document.getElementsByName('id')[0].value
     const name = document.getElementsByName('name')[0].value
     const ingredients = document.getElementsByName('ingredients')[0].value
     const details = document.getElementsByName('details')[0].value
-    if (name.trim() && ingredients && details) {
+    const category = document.getElementsByName('category')[0].value
+    const meal = document.getElementsByName('meal')[0].value
+    if (name.trim() && ingredients && details && category && meal) {
         const localData = JSON.parse(localStorage.getItem("data")) || []
         const index = localData.findIndex(item => item.id === id)
         localData[index]['id'] = id
         localData[index]['name'] = name
         localData[index]['ingredients'] = ingredients
         localData[index]['details'] = details
+        localData[index]['category'] = details
+        localData[index]['meal'] = details
         localStorage.setItem('data', JSON.stringify(localData))
         alert('Recipe has been Updated')
         window.location.replace("./recipies.html")
@@ -90,9 +91,11 @@ const handleAddItem = () => {
     const id = document.getElementsByName('id')[0].value
     const ingredients = document.getElementsByName('ingredients')[0].value
     const details = document.getElementsByName('details')[0].value
-    if (name.trim() && ingredients && details) {
+    const category = document.getElementsByName('category')[0].value
+    const meal = document.getElementsByName('meal')[0].value
+    if (name.trim() && ingredients && details && category && meal) {
         const localData = JSON.parse(localStorage.getItem("data")) || []
-        localData.push({ name, ingredients, details, id })
+        localData.push({ name, ingredients, details, id,meal, category })
         localStorage.setItem('data', JSON.stringify(localData))
         alert('Recipe has been added')
         window.location.replace("./recipies.html")
@@ -101,9 +104,6 @@ const handleAddItem = () => {
         alert('all fields are required!')
     }
 }
-
-
-
 
 const handeNextClick = (container) => {
     const slidesContainer = document.getElementById(container);
